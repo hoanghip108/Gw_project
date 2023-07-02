07e1ae5df5e43684d161583d5cb4a2b99be7099a
Đỗ Nguyễn Huy Hoàng <hoanghip108@gmail.com>
2023-07-02 11:31:51 +0700

    fix response code

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
-import { createUser, login, verifyUser } from "../services/userServices";
-import { UserSchema, Loginschema } from "../validators/userValidate";
-const httpStatus = require("http-status");
+import { createUser, login, verifyUser } from '../services/userServices';
+import { UserSchema, Loginschema } from '../validators/userValidate';
+const httpStatus = require('http-status');
 const {
   Common,
   Success,
@@ -13,7 +13,7 @@ const {
   Conflict,
   ValidateFailed,
   WrongUsernameOrpassWord,
-} = require("../helper/apiResponse");
+} = require('../helper/apiResponse');
 const createUserController = async (req, res, next) => {
   try {
     const { error, value } = UserSchema.validate(req.body);
@@ -26,7 +26,7 @@ const createUserController = async (req, res, next) => {
       return res.status(httpStatus.CONFLICT).json(new Conflict());
     }
     res.status(httpStatus.OK);
-    res.json(new Success("Data updated successfully.", user));
+    res.json(new Success('Data updated successfully.', user));
   } catch (err) {
     next(err);
   }
@@ -35,13 +35,13 @@ const loginController = async (req, res, next) => {
   try {
     const { error, value } = Loginschema.validate(req.body);
     if (error) {
-      return res.status(httpStatus.BAD_REQUEST).json(error.details[0].message);
+      return res.status(httpStatus.Unauthorized).json(error.details[0].message);
     }
     const token = await login(value);
     if (token == null) {
       res.status(httpStatus.UNAUTHORIZED).json(new WrongUsernameOrpassWord());
     }
-    res.status(httpStatus.OK).json(new Success("", token));
+    res.status(httpStatus.OK).json(new Success('', token));
   } catch (err) {
     next(err);
   }
@@ -49,17 +49,13 @@ const loginController = async (req, res, next) => {
 const verifyUserController = async (req, res, next) => {
   try {
     const protocol = req.protocol;
-    const host = req.get("host");
+    const host = req.get('host');
     const id = req.params.id;
     const verify = await verifyUser(id);
     if (verify) {
-      return res
-        .status(httpStatus.OK)
-        .json(new Success("your account has been verified"));
+      return res.status(httpStatus.OK).json(new Success('your account has been verified'));
     }
-    return res
-      .status(httpStatus.BadRequest)
-      .json(new BadRequest("email is not verified"));
+    return res.status(httpStatus.BadRequest).json(new BadRequest('email is not verified'));
   } catch (err) {
     next(err);
   }
